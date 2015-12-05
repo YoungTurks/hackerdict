76f3e9e84bdd78094103f51c7494a0691eae1a09
Ustun Ozgur <ustunozgur@gmail.com>
2015-12-05 14:34:34 +0200

    Move extra datomic files to snippets

--- project.clj
+++ project.clj
@@ -1,15 +1,25 @@
-(defproject hackerdict "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
-                                   :creds :gpg}}
-  :profiles {:dev {:dependencies [[alembic "0.3.2"]]}}
+(defproject hackerdict "1.0.0-SNAPSHOT"
+  :description "The community driven dictionary of hacker interests."
+  :url "http://hackerdict.herokuapp.com"
   :main hackerdict.web
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring "1.4.0"]
-                 [org.clojure/data.json "0.2.6"]
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :username [:env/datomic_username]
+                                   :password [:env/datomic_password]}}
+  :profiles {:dev {:dependencies [[alembic "0.3.2"]]}}
+  :dependencies [[com.cemerick/url "0.1.1"]
+                 [com.cemerick/drawbridge "0.0.7" :exclusions [ring/ring-core]]
+                 [com.datomic/datomic-pro "0.9.5206" :exclusions [joda-time]]
+                 [compojure "1.4.0"]
+                 [environ "1.0.1"]
+                 [http-kit "2.1.18"]
+                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/data.generators "0.1.2"]
-                 [com.datomic/datomic-pro "0.9.5206" :exclusions [joda-time]]])
+                 [org.clojure/tools.nrepl "0.2.11"]
+                 [ring-basic-authentication "1.0.5"]
+                 [ring/ring-devel "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "hackerdict-standalone.jar"
+  :profiles {:production {:env {:production true}}})
