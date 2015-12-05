7052b9b0ba9e8bd73ded48093e9fb50ad27e949a
Emrehan Tüzün <emrehan.tuzun@gmail.com>
2015-12-05 06:14:12 +0200

    Update project.clj
     * Update dependencies
     * Add datomic dependecies

--- hackerdict/project.clj
+++ hackerdict/project.clj
@@ -1,15 +1,17 @@
 (defproject hackerdict "1.0.0-SNAPSHOT"
   :description "The community driven dictionary of hacker interests."
   :url "http://hackerdict.herokuapp.com"
-  :license {:name "FIXME: choose"
-            :url "http://example.com/FIXME"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [ring/ring-devel "1.2.2"]
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
+  :dependencies [[com.cemerick/drawbridge "0.0.7"]
+                 [com.datomic/datomic-pro "0.9.5206"]
+                 [compojure "1.4.0"]
+                 [environ "1.0.1"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.generators "0.1.2"]
                  [ring-basic-authentication "1.0.5"]
-                 [environ "0.5.0"]
-                 [com.cemerick/drawbridge "0.0.6"]]
+                 [ring/ring-devel "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
