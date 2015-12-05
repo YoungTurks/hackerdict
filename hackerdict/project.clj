707cb6870481172a8b43b8277aaeea23f4965b83
Ustun Ozgur <ustunozgur@gmail.com>
2015-12-05 05:02:22 +0200

    Datomic examples

--- hackerdict/project.clj
+++ hackerdict/project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [com.datomic/datomic-pro "0.9.5206"]])
