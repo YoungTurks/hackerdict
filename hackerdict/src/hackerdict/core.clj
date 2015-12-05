d275abf8e60745f1e620184659cd2bae8ef22077
Ustun Ozgur <ustunozgur@gmail.com>
2015-12-05 03:04:43 +0200

    Initial commit

---
+++ hackerdict/src/hackerdict/core.clj
@@ -0,0 +1,6 @@
+(ns hackerdict.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ hackerdict/test/hackerdict/core_test.clj
@@ -0,0 +1,7 @@
+(ns hackerdict.core-test
+  (:require [clojure.test :refer :all]
+            [hackerdict.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
