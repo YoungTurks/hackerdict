a787e45b57f3b0f89c2c4f3c8f194262ed8cff28
Emrehan Tüzün <emrehan.tuzun@gmail.com>
2015-12-06 03:20:18 +0200

    Refactor web.clj using the new response function

--- src/hackerdict/web.clj
+++ src/hackerdict/web.clj
@@ -5,6 +5,7 @@
             [clojure.java.io :as io]
             [hackerdict.auth :as auth]
             [hackerdict.rest.user :refer [user-routes]]
+            [hackerdict.util.rest :as rest]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.basic-authentication :as basic]
             [ring.middleware.reload :as reload]
@@ -29,61 +30,55 @@
 
   (GET "/login" {session :session}
     (if-let [token (:token session)]
-      {:status 404
-       :headers {"Content-Type" "text/plain"}
-       :body "Already logged in."}
+      (rest/response {:status 400
+                      :body   "Already logged in."})
       (let [state (auth/random-state)
             uri (auth/authorize-uri state)]
-         {:status 302 
-          :headers {"Location" uri}
-          :session (assoc session :state state)})))
+         (rest/response {:status  302 
+                         :headers {"Location" uri}
+                         :session (assoc session :state state)}))))
 
   (GET "/logout" {session :session}
     (if-let [token (:token session)]
-      {:status 302 
-       :headers {"Location" "/"}
-       :session (dissoc session :token)}
-      {:status 404
-       :headers {"Content-Type" "text/plain"}
-       :body "Not logged in."}))
+      (rest/response {:status  302 
+                      :headers {"Location" "/"}
+                      :session (dissoc session :token)})
+      (rest/response {:status 400 
+                      :body   "Not logged in."})))
   
-  (ANY "/auth" {params :params session :session}
+  (GET "/auth" {params :params session :session}
     (if (= (:state params) (:state session))
       (if-let [token (auth/access-token (:code params))]
-        {:status 302 
-         :headers {"Location" "/"}
-         :session (assoc session :token token)}
-        {:status 400
-         :headers {"Content-Type" "text/plain"}
-         :body "Cannot get token."})
-      {:status 400
-       :headers {"Content-Type" "text/plain"}
-       :body    "Sessions doesn't match."}))
+        (rest/response {:status  302 
+                        :headers {"Location" "/"}
+                        :session (assoc session :token token)})
+        (rest/response {:status 500
+                        :body   "Cannot get token."}))
+      (rest/response {:status 400
+                      :body   "Sessions doesn't match."})))
+
+  (GET "/" {session :session}
+    (rest/response {:body (str "Main Page \n"
+                               "========= \n"
+                               (when-let [token (:token session)] 
+                                 (println (str "at root token is " token))
+                                 (str "User token is " token ".\n"
+                                      "Username is " (auth/get-username token) ".\n" 
+                                      "Name is " (auth/get-name token) ".\n" 
+                                      "Email is " (auth/get-email token) ".\n")))}))
 
   (ANY "/repl" {:as req}
        (drawbridge req))
-  
-  (GET "/" {session :session}
-    {:status 200
-     :headers {"Content-Type" "text/plain"}
-     :body (str "Main Page \n"
-                "========= \n"
-              (when-let [token (:token session)] 
-                (str "User token is " token ".\n"
-                     "Username is " (auth/get-username token) ".\n" 
-                     "Name is " (auth/get-name token) ".\n" 
-                     "Email is " (auth/get-email token) ".\n")))})
-  
+                    
   (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+    (route/not-found (slurp (io/resource "404.html")))))
 
 (defn wrap-error-page [handler]
   (fn [req]
     (try (handler req)
-         (catch Exception e
-           {:status 500
-            :headers {"Content-Type" "text/html"}
-            :body (slurp (io/resource "500.html"))}))))
+      (catch Exception e
+        (rest/response {:status 500
+                        :body (slurp (io/resource "500.html"))})))))
 
 (defn wrap-app [app]
   ;; TODO: heroku config:add SESSION_SECRET=$RANDOM_16_CHARS
